105f86a357cd05cccc2b6afa4dd167a893f5c38a
choisohyun <sohyun4283@naver.com>
2020-04-21 23:19:49 +0900

    [#31] feat: 컴포넌트를 가지고 있는 App 스켈레톤 작성

--- FE/sidedish/src/App.js
+++ FE/sidedish/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Header from "./components/Header/Header";
+import Category from "./components/Category/Category";
+import ProductCarousel from "./components/ProductCarousel/ProductCatousel";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Header></Header>
+      <Category></Category>
+      <ProductCarousel></ProductCarousel>
     </div>
   );
-}
+};
 
 export default App;
