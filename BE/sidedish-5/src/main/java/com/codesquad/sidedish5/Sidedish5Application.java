9a7c03d9d4e1609153a8cfcd93d5100ae89c1a18
suhyunsim <cnc1106@naver.com>
2020-04-21 15:45:51 +0900

    [#14] chore: 프로젝트 기본 세팅
    dependencies 추가

---
+++ BE/sidedish-5/src/main/java/com/codesquad/sidedish5/Sidedish5Application.java
@@ -0,0 +1,13 @@
+package com.codesquad.sidedish5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sidedish5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sidedish5Application.class, args);
+	}
+
+}
