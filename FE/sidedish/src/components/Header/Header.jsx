c0f748f8106e4ed6b87f23ad7b79271626a89eb9
choisohyun <sohyun4283@naver.com>
2020-04-21 23:15:09 +0900

    [#31] feat: Header 영역 컴포넌트 스켈레톤 코드 작성

--- FE/sidedish/src/components/Header/Header.jsx
+++ FE/sidedish/src/components/Header/Header.jsx
@@ -1,1 +1,19 @@
 import React from "react";
+import { jsx, css } from "@emotion/core";
+import styled from "@emotion/styled";
+
+const headerMenuList = [];
+
+const HeaderBlock = styled.div``;
+
+const HeaderMenu = styled.div``;
+
+const Header = () => {
+  return (
+    <HeaderBlock>
+      <HeaderMenu></HeaderMenu>
+    </HeaderBlock>
+  );
+};
+
+export default Header;
