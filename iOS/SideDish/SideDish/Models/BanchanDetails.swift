7d19f03fd71393f904ed55ea2119ce7f9f15b007
idevjinie <idevjinie@gmail.com>
2020-04-21 14:49:10 +0900

    [#25] feat: 반찬 데이터 관련 구조체 정의

---
+++ iOS/SideDish/SideDish/Models/BanchanDetails.swift
@@ -0,0 +1,29 @@
+//
+//  BanchanDetails.swift
+//  SideDish
+//
+//  Created by jinie on 2020/04/21.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import Foundation
+
+struct BanchanDetailsInformation: Codable {
+    
+    let hash: String
+    let data: BanchanDetails
+    
+}
+
+struct BanchanDetails: Codable {
+    
+    let top_image: String
+    let thumb_images: [String]
+    let product_description: String
+    let point: String
+    let delivery_info: String
+    let delivery_fee: String
+    let prices: [String]
+    let detail_section: [String]
+    
+}
