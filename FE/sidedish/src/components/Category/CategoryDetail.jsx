fcce4bd196a5facfeb6f555e39785519dee5abd0
choisohyun <sohyun4283@naver.com>
2020-04-21 23:15:51 +0900

    [#31] feat: Category 영역 컴포넌트 스켈레톤 코드 작성

--- FE/sidedish/src/components/Category/CategoryDetail.jsx
+++ FE/sidedish/src/components/Category/CategoryDetail.jsx
@@ -1,1 +1,13 @@
 import React from "react";
+import { jsx, css } from "@emotion/core";
+import styled from "@emotion/styled";
+
+const CategoryDetail = () => {
+  return (
+    <div>
+      <ul></ul>
+    </div>
+  );
+};
+
+export default CategoryDetail;
--- FE/sidedish/src/components/Category/CategoryList.jsx
+++ FE/sidedish/src/components/Category/CategoryList.jsx
@@ -1,1 +1,14 @@
 import React from "react";
+import { jsx, css } from "@emotion/core";
+import styled from "@emotion/styled";
+import Category from "./Category";
+
+const CategoryLsit = () => {
+  return (
+    <div>
+      <Category></Category>
+    </div>
+  );
+};
+
+export default CategoryList;
