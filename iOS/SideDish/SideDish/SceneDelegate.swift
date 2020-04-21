76df9170ac830e7b697fe0cc2e62e8d915cfd95f
idevjinie <idevjinie@gmail.com>
2020-04-21 10:34:02 +0900

    [#24] chore: Toaster 패키지 설치

---
+++ iOS/SideDish/SideDish/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  SideDish
+//
+//  Created by jinie on 2020/04/20.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
---
+++ iOS/SideDish/SideDish/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SideDish
+//
+//  Created by jinie on 2020/04/20.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
