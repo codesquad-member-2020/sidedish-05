b8535a8e3c3ab7cca98e9b8f3d8e88120b53be62
choisohyun <sohyun4283@naver.com>
2020-04-22 21:43:45 +0900

    [#3] feat: mouse event로 hover 상태에 맞게 상태 관리 코드 작성

--- FE/sidedish/src/components/Category/Category.jsx
+++ FE/sidedish/src/components/Category/Category.jsx
@@ -1,59 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
 import CategoryDetail from "./CategoryDetail";
-
-const categories = [
-  {
-    name: "sidedish",
-    title: "밑반찬",
-  },
-  {
-    name: "soup",
-    title: "국ㆍ찌개",
-  },
-  {
-    name: "maindish",
-    title: "메인반찬",
-  },
-  {
-    name: "kiddish",
-    title: "아이반찬",
-  },
-  {
-    name: "reqularmeal",
-    title: "정기식단",
-  },
-  {
-    name: "fresh",
-    title: "신선ㆍ가공",
-  },
-  {
-    name: "snack",
-    title: "간식",
-  },
-  {
-    name: "brand",
-    title: "브랜드관",
-  },
-];
-
-const NaviWrap = styled.div`
-  position: relative;
-  z-index: 100;
-  background: #483f35;
-  align-items: center;
-  display: flex;
-  height: 52px;
-`;
-
-const CategoryBlock = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 980px;
-  height: inherit;
-  margin: 0 auto;
-  overflow: visible;
-`;
 
 const Category = styled.div`
   height: 95%;
@@ -71,29 +18,23 @@ const Category = styled.div`
     box-sizing: border-box;
     transform: translateY(5%);
   }
-  &.active {
-    font-weight: 600;
-    border-bottom: 2px solid #22b8cf;
-    color: #22b8cf;
-    &:hover {
-      color: #3bc9db;
-    }
-  }
   & + & {
     margin-left: 1rem;
   }
 `;
 
-const Categories = () => {
+const Categories = ({ category }) => {
+  const [hover, setHover] = useState(false);
+
+  const toggleHover = () => setHover(!hover);
+
   return (
-    <NaviWrap>
-      <CategoryBlock>
-        {categories.map((category) => (
-          <Category key={category.name}>{category.title}</Category>
-        ))}
-        <CategoryDetail></CategoryDetail>
-      </CategoryBlock>
-    </NaviWrap>
+    <div>
+      <Category onMouseEnter={toggleHover} onMouseLeave={toggleHover} key={category.name}>
+        {category.title}
+        {hover && <CategoryDetail contents={category.contents}></CategoryDetail>}
+      </Category>
+    </div>
   );
 };
 
