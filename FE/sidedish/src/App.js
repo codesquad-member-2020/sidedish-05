397696595d588cfa204e71c656011032115617d8
choisohyun <sohyun4283@naver.com>
2020-04-22 18:18:36 +0900

    [#3] feat: Global style App.js에 적용하는 코드 추가

--- FE/sidedish/src/App.js
+++ FE/sidedish/src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
-import Header from "./components/Header/Header";
+// import Header from "./components/Header/Header";
 import Category from "./components/Category/Category";
-import ProductCarousel from "./components/ProductCarousel/ProductCatousel";
+// import ProductCarousel from "./components/ProductCarousel/ProductCatousel";
+import reset from "./Reset";
+import { Global } from "@emotion/core";
 
 const App = () => {
   return (
-    <div className="App">
-      <Header></Header>
+    <div>
+      <Global styles={reset} />
+      {/* <Header></Header> */}
       <Category></Category>
-      <ProductCarousel></ProductCarousel>
+      {/* <ProductCarousel></ProductCarousel> */}
     </div>
   );
 };
