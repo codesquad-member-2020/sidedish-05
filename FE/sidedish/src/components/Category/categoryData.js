11265190376e370c9de55c2d444be0c98eed6527
choisohyun <sohyun4283@naver.com>
2020-04-22 21:29:34 +0900

    [#3] feat: 카테고리 정보 별도 파일로 분리

---
+++ FE/sidedish/src/components/Category/categoryData.js
@@ -0,0 +1,66 @@
+const categoryData = [
+  {
+    name: "sidedish",
+    title: "밑반찬",
+    contents: [
+      "무침",
+      "나물무침",
+      "볶음",
+      "조림",
+      "김치",
+      "전",
+      "장아찌·피클",
+      "젓갈·장·소스",
+      "세트",
+    ],
+  },
+  {
+    name: "soup",
+    title: "국ㆍ찌개",
+    contents: ["국", "찌개", "탕", "전골", "세트"],
+  },
+  {
+    name: "maindish",
+    title: "메인반찬",
+    contents: [
+      "고기반찬",
+      "해산물반찬",
+      "생선반찬",
+      "덮밥",
+      "튀김",
+      "면",
+      "양식",
+      "아시아식",
+      "분식",
+      "죽",
+      "세트",
+    ],
+  },
+  {
+    name: "kiddish",
+    title: "아이반찬",
+    contents: ["이유식 초기/중기", "이유식 후기/완료기", "아이반찬", "어린이반찬", "간식·음료"],
+  },
+  {
+    name: "reqularmeal",
+    title: "정기식단",
+    contents: ["1~2인", "3~4인", "아이반찬"],
+  },
+  {
+    name: "fresh",
+    title: "신선ㆍ가공",
+    contents: ["간편반찬", "간편국찌개", "간편식품"],
+  },
+  {
+    name: "snack",
+    title: "간식",
+    contents: ["베이커리", "과일", "주스", "스무디", "유제품·커피", "기타간식"],
+  },
+  {
+    name: "brand",
+    title: "브랜드관",
+    contents: ["반찬가게", "반찬장인", "셰프의요리", "전국맛집", "키즈관"],
+  },
+];
+
+export default categoryData;
