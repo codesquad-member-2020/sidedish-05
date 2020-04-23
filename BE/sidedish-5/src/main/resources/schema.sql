4828ccd0c8d4ba0116882a7e2fb63b670a47f24a
suhyunsim <cnc1106@naver.com>
2020-04-23 15:01:32 +0900

    [#15] refactor: db schema에 delivery_type table 추가 및 테이블 오타 수정

--- BE/sidedish-5/src/main/resources/schema.sql
+++ BE/sidedish-5/src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS user, bestCategory_sidedish, bestCategory, detailImage, thumbImage, sidedish_badge, sidedish, badge, category;
+DROP TABLE IF EXISTS user, bestCategory_sidedish, bestCategory, detail_image, thumb_image, sidedish_badge, sidedish, badge, category;
 
 CREATE TABLE category (
     id int auto_increment primary key,
@@ -19,7 +19,7 @@ CREATE TABLE sidedish (
     n_price int,
     s_price int,
     delivery_type varchar (64),
-    delivery_info varchar (64),
+    delivery_info varchar (255),
     category varchar (64) REFERENCES category(id) ON UPDATE CASCADE
 );
 
@@ -29,14 +29,14 @@ CREATE TABLE sidedish_badge (
     PRIMARY KEY (badge, sidedish)
 );
 
-CREATE TABLE thumbImage (
+CREATE TABLE thumb_image (
     id int auto_increment primary key,
     sidedish varchar (64) REFERENCES sidedish(id) ON UPDATE CASCADE,
     sidedish_key int,
     image_url varchar (255)
 );
 
-CREATE TABLE detailImage (
+CREATE TABLE detail_image (
     id int auto_increment primary key,
     sidedish varchar (64) REFERENCES sidedish(id) ON UPDATE CASCADE,
     sidedish_key int,
@@ -47,15 +47,28 @@ CREATE TABLE bestCategory (
     id int auto_increment primary key,
     name varchar (64)
 );
+
 CREATE TABLE bestCategory_sidedish (
     best_category int REFERENCES bestCategory(id) ON UPDATE CASCADE,
     sidedish varchar (64) REFERENCES sidedish(id) ON UPDATE CASCADE,
     PRIMARY KEY (best_category, sidedish)
 );
+
+CREATE TABLE delivery_type {
+    id int auto_increment primary key,
+    delivery_type varchar (64)
+}
+
+CREATE TABLE sidedish_delivery_type (
+    delivery_type int REFERENCES delivery_type(id) ON UPDATE CASCADE,
+    sidedish varchar (64) REFERENCES sidedish(id) ON UPDATE CASCADE,
+    PRIMARY KEY (delivery_type, sidedish)
+);
+
 CREATE TABLE user (
     id int auto_increment primary key,
     email varchar (64),
     password varchar (64),
-    accessToken varchar (255),
-    refreshToken varchar (255)
+    access_token varchar (255),
+    refresh_token varchar (255)
 );
