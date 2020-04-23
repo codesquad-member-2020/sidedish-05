40ba0c1cbb0df799dab7dcc97ea0fc00a533aa1e
suhyunsim <cnc1106@naver.com>
2020-04-23 15:06:23 +0900

    [#36] feat: CategoryRepository 추가

---
+++ BE/sidedish-5/src/main/java/com/codesquad/sidedish5/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.codesquad.sidedish5.repository;
+
+import com.codesquad.sidedish5.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Integer> {
+
+}
