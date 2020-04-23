04fab583cd755660e961556f33c000dd4a422b10
idevjinie <idevjinie@gmail.com>
2020-04-23 22:44:01 +0900

    [#25] feat: BadgeLabel 클래스에 inset 설정 추가

--- iOS/SideDish/SideDish/Views/BadgeLabel.swift
+++ iOS/SideDish/SideDish/Views/BadgeLabel.swift
@@ -9,15 +9,28 @@
 import UIKit
 
 class BadgeLabel: UILabel {
+    let colors: [String : UIColor] = ["이벤트특가": .systemPurple, "론칭특가": .systemOrange, "품절": .black]
+    let insets = UIEdgeInsets(top: 1, left: 2, bottom: 1, right: 2)
+    
     init(title: String) {
         super.init(frame: .zero)
         text = title
         textColor = .white
         font = .systemFont(ofSize: 13)
-        backgroundColor = .systemPurple
+        backgroundColor = colors[title]
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: insets))
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        return CGSize(width: size.width + insets.left + insets.right,
+                      height: size.height + insets.top + insets.bottom)
     }
 }
