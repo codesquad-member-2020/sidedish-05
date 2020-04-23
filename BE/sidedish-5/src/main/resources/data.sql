169324ef7b5e6e740a9542997f1032b3e99295e6
suhyunsim <cnc1106@naver.com>
2020-04-23 18:23:28 +0900

    [#36] feat: Many to many 연결을 위해 data.sql에 데이터 추가

---
+++ BE/sidedish-5/src/main/resources/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO badge
+    (id, name)
+VALUES (1, '이벤트특가'),
+       (2, '론칭특가'),
+       (3, '이벤트 특가'),
+       (4, '베스트'),
+       (5, '품절');
+
+INSERT INTO delivery_type
+    (id, delivery_type)
+VALUES (1, '새벽배송'),
+       (2, '전국택배');
+
+INSERT INTO bestCategory
+    (id, name)
+VALUES (1, '할인특가 세트상'),
+       (2, '풍성한 고기반찬'),
+       (3, '바다향가득 반찬'),
+       (4, '편리한 반찬세트'),
+       (5, '간편한 덮밥요리');
