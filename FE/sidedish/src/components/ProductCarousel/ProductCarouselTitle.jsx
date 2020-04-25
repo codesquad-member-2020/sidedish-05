f62ee830431ec68cb97f9b4a9b2534369da58ed6
choisohyun <sohyun4283@naver.com>
2020-04-25 17:33:01 +0900

    [#4] feat: 캐러셀 타이틀 스타일 추가

--- FE/sidedish/src/components/ProductCarousel/ProductCarouselTitle.jsx
+++ FE/sidedish/src/components/ProductCarousel/ProductCarouselTitle.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-const Container = styled.div``;
+const Container = styled.div`
+  margin: 70px 0 32px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
 
-const Title = styled.h2``;
+const Title = styled.h2`
+  color: #999;
+  font-size: 18px;
+  margin-bottom: 5px;
+`;
 
-const Description = styled.h3``;
+const Description = styled.p`
+  font-size: 34px;
+`;
 
 const ProductCarouselTitle = ({ title, description }) => {
   return (
