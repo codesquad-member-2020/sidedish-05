4b8bd7bd73b9bc4ee7439cf1266843773e3c96f7
choisohyun <sohyun4283@naver.com>
2020-04-25 10:20:26 +0900

    [#4] feat: api 주소로 get 요청보내서 데이터를 Product로 내려주기
    - fetch 함수는 추후 별도 파일로 분리 예정임

--- FE/sidedish/src/components/ProductCarousel/ProductCatousel.jsx
+++ FE/sidedish/src/components/ProductCarousel/ProductCatousel.jsx
@@ -1,18 +1,41 @@
-import React from "react";
-import { jsx, css } from "@emotion/core";
+import React, { useEffect, useState } from "react";
 import styled from "@emotion/styled";
+import axios from "axios";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Skeleton from "react-loading-skeleton";
 import Product from "./Product";
+// import usePromise from "../../lib/usePromise";
+import arrowNext from "./navigate_next.png";
+import arrowPrev from "./navigate_before.png";
 
-const ProductCarousel = () => {
+const ProductCarousel = ({ api }) => {
+  const [products, setProducts] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const response = await axios.get(api);
+      setProducts(response.data.body);
+      setLoading(false);
+    };
+    fetchData();
+  }, [api]);
+
+  if (!products) return null;
+
   return (
-    <div>
+    <>
       <Slider>
-        <div>
-          <Product></Product>
-        </div>
+        {products.map((item) => (
+          <div style={{ width: 215 }}>
+            <Product item={item} key={item.detail_hash}></Product>
+          </div>
+        ))}
       </Slider>
-    </div>
+    </>
   );
 };
 
