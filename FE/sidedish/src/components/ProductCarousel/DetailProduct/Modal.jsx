db8d990ead61c0d633fb2ff0641ae4d01a70b092
choisohyun <sohyun4283@naver.com>
2020-04-26 11:29:15 +0900

    [#7] 모달창 영역 잡을 수 있도록 스타일 추가

--- FE/sidedish/src/components/ProductCarousel/DetailProduct/Modal.jsx
+++ FE/sidedish/src/components/ProductCarousel/DetailProduct/Modal.jsx
@@ -3,9 +3,28 @@ import ReactDOM from "react-dom";
 import styled from "@emotion/styled";
 import DetailProduct from "./DetailProduct";
 
-const ModalOverlay = styled.div``;
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1040;
+  width: 100vw;
+  height: 100vh;
+  background-color: #000;
+  opacity: 0.5;
+`;
 
-const ModalWrapper = styled.div``;
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1050;
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+  outline: 0;
+`;
 
 const ModalDetail = styled.div``;
 
