70029ec166cb2fcf4977281e3994d35a76233072
choisohyun <sohyun4283@naver.com>
2020-04-26 11:25:26 +0900

    [#7] feat: API URL에 detail(상세 페이지 정보) 추가

--- FE/sidedish/src/common/config.js
+++ FE/sidedish/src/common/config.js
@@ -2,6 +2,7 @@ export const API_URL = {
   main: process.env.REACT_APP_BASE_URL + process.env.REACT_APP_API_MAIN,
   soup: process.env.REACT_APP_BASE_URL + process.env.REACT_APP_API_SOUP,
   side: process.env.REACT_APP_BASE_URL + process.env.REACT_APP_API_SIDE,
+  detail: process.env.REACT_APP_BASE_URL + process.env.REACT_APP_API_DETAIL,
 };
 
 export const API_NAME = ["main", "soup", "side"];
