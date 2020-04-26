fe1589d965c9aa66d0fb59e831f53fccf0749151
choisohyun <sohyun4283@naver.com>
2020-04-26 11:34:47 +0900

    [#7] feat: hash 정보 받아서 GET 요청보내서 데이터 받는 코드 추가

--- FE/sidedish/src/components/ProductCarousel/DetailProduct/DetailProduct.jsx
+++ FE/sidedish/src/components/ProductCarousel/DetailProduct/DetailProduct.jsx
@@ -1,14 +1,28 @@
-import React from "react";
-import { jsx, css } from "@emotion/core";
-import styled from "@emotion/styled";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import ImageList from "./ImageList";
 import ProductInfo from "./ProductInfo";
 import RecommandProductList from "./RecommandProductList";
+import { API_URL } from "../../../common/config";
 
-const DetailProduct = () => {
+const DetailProduct = ({ hash }) => {
+  const [info, setInfo] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`${API_URL.detail}${hash}`);
+      console.log(response.data.data);
+      setInfo(response.data.data);
+    };
+    fetchData();
+  }, [hash]);
+
+  if (!info) return null;
+  console.log(info);
+
   return (
     <div>
-      <ImageList></ImageList>
+      <ImageList imagedata={info.top_image}></ImageList>
       <ProductInfo></ProductInfo>
       <RecommandProductList></RecommandProductList>
     </div>
