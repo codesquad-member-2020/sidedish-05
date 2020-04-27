fa6182f4246523d01bed2eebffa9432722577a3e
sohyun4283 <sohyun4283@naver.com>
2020-04-27 22:51:55 +0900

    [#53] feat: 썸네일 이미지 리스트로 작게 보이도록 렌더링 처리

--- FE/sidedish/src/components/ProductCarousel/DetailProduct/ImageList.jsx
+++ FE/sidedish/src/components/ProductCarousel/DetailProduct/ImageList.jsx
@@ -7,12 +7,30 @@ const ImageListContainer = styled.div`
 
 const Image = styled.img`
   width: 90%;
+  margin-bottom: 5px;
+`;
+
+const ThumbnailContainer = styled.div`
+  width: 90%;
+`;
+
+const Thumbnail = styled.img`
+  width: 24%;
+  background-color: #5d5d5d;
+  & + & {
+    margin-left: 1%;
+  }
 `;
 
 const ImageList = ({ topImage, thumbImage }) => {
   return (
     <ImageListContainer>
       <Image src={topImage}></Image>
+      <ThumbnailContainer>
+        {thumbImage.map((image, index) => (
+          <Thumbnail src={image} key={index} />
+        ))}
+      </ThumbnailContainer>
     </ImageListContainer>
   );
 };
