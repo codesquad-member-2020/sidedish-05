7f03d01357d998b7d240f1ae1a00cb11dd09daf1
sohyun4283 <sohyun4283@naver.com>
2020-04-27 20:31:16 +0900

    [#2] feat: Header 컴포넌트에 LocalNaviBar 컴포넌트 불러오기

--- FE/sidedish/src/components/Header/Header.jsx
+++ FE/sidedish/src/components/Header/Header.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import styled from "@emotion/styled";
+import LocalNaviBar from "./LocalNaviBar";
 
-const headerMenuList = [];
+const HeaderBlock = styled.div`
+  width: 100%;
+`;
 
-const HeaderBlock = styled.div``;
+const HeaderMenu = styled.div`
+  border-bottom: 1px solid #e9e9e9;
+`;
 
-const HeaderMenu = styled.div``;
+const HeaderMain = styled.div``;
 
 const Header = () => {
   return (
     <HeaderBlock>
-      <HeaderMenu></HeaderMenu>
+      <HeaderMenu>
+        <LocalNaviBar></LocalNaviBar>
+      </HeaderMenu>
+      <HeaderMain></HeaderMain>
     </HeaderBlock>
   );
 };
