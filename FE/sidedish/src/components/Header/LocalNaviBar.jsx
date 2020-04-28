47997cd4e7b7729a0449a75e96373302de36a87b
sohyun4283 <sohyun4283@naver.com>
2020-04-28 16:08:37 +0900

    [#50] feat: 로그인만 OAuth 링크 추가

--- FE/sidedish/src/components/Header/LocalNaviBar.jsx
+++ FE/sidedish/src/components/Header/LocalNaviBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
 import headerData from "./headerData";
+import { AUTH_URL } from "../../common/config";
 
 const LocalNaviBarContainer = styled.div`
   display: flex;
@@ -11,7 +12,7 @@ const LocalNaviBarContainer = styled.div`
   margin: 0 auto;
 `;
 
-const Navigation = styled.div`
+const Navigation = styled.a`
   padding: 10px 10px 9px 10px;
   height: 17px;
   font-size: 12px;
@@ -32,9 +33,15 @@ const Navigation = styled.div`
 const LocalNaviBar = () => {
   return (
     <LocalNaviBarContainer>
-      {headerData.map((header, index) => (
-        <Navigation key={index}>{header.name}</Navigation>
-      ))}
+      {headerData.map((header, index) =>
+        index === 0 ? (
+          <Navigation href={AUTH_URL} key={index}>
+            {header.name}
+          </Navigation>
+        ) : (
+          <Navigation key={index}>{header.name}</Navigation>
+        )
+      )}
     </LocalNaviBarContainer>
   );
 };
